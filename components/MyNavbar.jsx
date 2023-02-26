1101fac19d53c77205978c9ac2840c3aa71c4a5c
Shubham Kumar <55661658+ShubhamxKumar@users.noreply.github.com>
2023-02-26 12:02:53 +0530

    updates

---
+++ components/MyNavbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import right from "../imgs/right.png";
+
+function MyNavbar() {
+  return (
+    <Navbar bg="transparent" sticky="top">
+      <Container fluid className="justify-content-center">
+        <Navbar.Brand
+          href="/"
+          className="fw-bold fs-1"
+          style={{ color: "#2160fd" }}
+        >
+          Proxley
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link
+            className="text-light pb-0 fs-5"
+            style={{ fontFamily: "regular" }}
+          >
+            Pricing
+          </Nav.Link>
+        </Nav>
+        <Nav.Link
+          className="btn py-2 px-4 br-2 fs-6 d-flex flex-row justify-content-center btn-hover"
+          style={{ fontFamily: "regular" }}
+          href="/register"
+        >
+          Get Started <img src={right.src} />
+        </Nav.Link>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MyNavbar;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, Container, Nav, Navbar } from "react-bootstrap";
-import styles from "../styles/Home.module.css";
+import { Button, Col, Container, Nav, Navbar } from "react-bootstrap";
+import MyNavbar from "../components/MyNavbar";
 import bg from "../imgs/bg.png";
 import right from "../imgs/right.png";
 
@@ -11,32 +11,32 @@ export default function Home() {
       style={{ background: `url(${bg.src})` }}
       className="min-vh-100"
     >
-      <Navbar bg="transparent" sticky="top">
-        <Container fluid className="justify-content-center">
-          <Navbar.Brand
-            href="/"
-            className="fw-bold fs-1"
-            style={{ color: "#2160fd" }}
-          >
-            Proxley
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link
-              className="text-light pb-0 fs-5"
-              style={{ fontFamily: "regular" }}
-            >
-              Pricing
-            </Nav.Link>
-          </Nav>
-          <Nav.Link
-            className="btn py-2 px-4 br-2 fs-6 d-flex flex-row justify-content-center btn-hover"
-            style={{ fontFamily: "regular" }}
-            href="/register"
-          >
-            Get Started <img src={right.src} />
-          </Nav.Link>
-        </Container>
-      </Navbar>
+      <MyNavbar />
+      <Container
+        fluid
+        style={{ height: "60vh", gap: "1rem" }}
+        className="d-flex flex-column justify-content-center d-flex w-100 align-items-center"
+      >
+        <h1
+          style={{ fontFamily: "bold", fontSize: "4rem" }}
+          className="text-light m-0 text-center w-75"
+        >
+          Highest quality Customisable Printing and Packaging
+        </h1>
+        <h3
+          style={{ fontFamily: "regular" }}
+          className="text-muted w-50 text-center"
+        >
+          We design , print and manufacture boxes all under one roof.
+        </h3>
+        <Nav.Link
+          className="btn py-2 px-4 br-2 fs-6 d-flex flex-row justify-content-center btn-hover"
+          style={{ fontFamily: "regular" }}
+          href="/register"
+        >
+          Get Started <img src={right.src} />
+        </Nav.Link>
+      </Container>
     </Container>
   );
 }
